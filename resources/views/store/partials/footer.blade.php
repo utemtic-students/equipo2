e17d27a767c8edc33ec20fc2d42f2b14f6b7f5d4
CristianVillalvazo <a20130503@utem.edu.mx>
2016-06-23 18:16:28 -0500

    Subi Vistas del Footer

---
+++ resources/views/store/partials/footer.blade.php
@@ -0,0 +1,42 @@
+<footer class="container-fluid">
+	<div class="row">
+		<div class="col-md-4">
+			<h3>Inteligent Container</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolores ratione minus, voluptates dolore soluta harum. Nobis neque doloribus corrupti iusto rem beatae libero, incidunt animi nemo voluptatem dolores accusantium magni ad ipsum eum repellendus vero deleniti velit molestiae quaerat, veniam ducimus labore sapiente? Commodi ad, pariatur. Ab consectetur, quasi!</p>
+		</div>
+		<div class="col-md-4">
+			<h3>Desarrollado por:</h3>
+			<div class="author-info">
+				<img src="https://pbs.twimg.com/profile_images/743519149897330692/eR4y5lk_.jpg" alt="" class="avatar">
+				<p><a href="#">@TICS</a> Ingenieros en tecnologias de la informacion y comunicacion, area sistemas informaticos
+				<li>Cristian Alberto Rosas Villalvazo</li>
+				<li>Jordan Axel Ramirez Ramirez</li>
+				<li>Jessica Rubi Rubio Alapizco</li>
+				<li>Jose Antonio Lopez Perez</li>
+				</p>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<h3>Siguenos</h3>
+			<ul class="redes">
+				<li>
+					<a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+				</li>
+				<li>
+					<a href="https://twitter.com/InteligentCon"><i class="fa fa-twitter-square fa-2x"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
+				</li>
+				<li>
+					<a href="#"><i class="fa fa-youtube-square fa-2x"></i></a>
+				</li>
+			</ul>
+			<h3>Escribenos:</h3>
+			<i class="fa fa-at"></i> <a href="#">a20130503@utem.edu.mx</a>
+		</div>
+	</div>
+</footer>
---
+++ resources/views/store/partials/slider.blade.php
@@ -0,0 +1,40 @@
+<div id="slider" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#slider" data-slide-to="0" class="active"></li>
+    <li data-target="#slider" data-slide-to="1"></li>
+    <li data-target="#slider" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="http://freetopwallpaper.com/wp-content/gallery/bottle/bottle-design-wallpaper-hd-72.jpg" alt="slide1">
+        <div class="carousel-caption">
+          Slide 1
+        </div>
+      </div>
+      <div class="item">
+        <img src="http://previews.123rf.com/images/mathisa/mathisa1206/mathisa120600053/14224702-Parte-trasera-de-cinco-grandes-contenedores-de-basura-con-ruedas-y-de-reciclaje-Foto-de-archivo.jpg" alt="slide2">
+        <div class="carousel-caption">
+          Slide 2
+        </div>
+      </div>
+      <div class="item">
+        <img src="http://previews.123rf.com/images/leonardo255/leonardo2551411/leonardo255141100086/33354429-Bote-de-basura-situado-en-el-fondo-blanco-vector-Foto-de-archivo.jpg" alt="slide3">
+        <div class="carousel-caption">
+          Slide 3
+        </div>
+      </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#slider" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#slider" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div><hr>
