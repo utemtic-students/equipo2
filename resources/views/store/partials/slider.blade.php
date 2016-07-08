22f873cb107fd0ea17f1eba948c08fa6a5f64d0c
RubiiBampersii <a20130235@utem.edu.mx>
2016-07-08 16:28:49 -0500

    Imagenes deslyder
    las 4 imagenes oficiales

--- resources/views/store/partials/slider.blade.php
+++ resources/views/store/partials/slider.blade.php
@@ -4,26 +4,34 @@
     <li data-target="#slider" data-slide-to="0" class="active"></li>
     <li data-target="#slider" data-slide-to="1"></li>
     <li data-target="#slider" data-slide-to="2"></li>
+    <li data-target="#slider" data-slide-to="3"></li>
   </ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
       <div class="item active">
-        <img src="http://freetopwallpaper.com/wp-content/gallery/bottle/bottle-design-wallpaper-hd-72.jpg" alt="slide1">
+        <img src="{{sddet('image/InteligentContainerPasto.jpg')}}" alt="slide1">
         <div class="carousel-caption">
           Slide 1
         </div>
       </div>
       <div class="item">
-        <img src="http://previews.123rf.com/images/mathisa/mathisa1206/mathisa120600053/14224702-Parte-trasera-de-cinco-grandes-contenedores-de-basura-con-ruedas-y-de-reciclaje-Foto-de-archivo.jpg" alt="slide2">
+        <img src="{{sddet('image/10DeDescuento.jpg')}}" alt="slide2">
         <div class="carousel-caption">
           Slide 2
         </div>
       </div>
       <div class="item">
-        <img src="http://previews.123rf.com/images/leonardo255/leonardo2551411/leonardo255141100086/33354429-Bote-de-basura-situado-en-el-fondo-blanco-vector-Foto-de-archivo.jpg" alt="slide3">
+        <img src="{{sddet('image/IntelligentcontainerDesenfocado.jpg')}}" alt="slide3">
         <div class="carousel-caption">
           Slide 3
+        </div>
+      </div>
+      
+        <div class="item">
+        <img src="{{sddet('image/IntelligentcontainerDesenfocado2.jpg')}}" alt="slide3">
+        <div class="carousel-caption">
+          Slide 4
         </div>
       </div>
   </div>
