257f2d00a61fe11e4397d53b395d56df469e6c59
RubiiBampersii <a20130235@utem.edu.mx>
2016-07-08 17:11:00 -0500

    clase tic
    lectura de la empresa Quienes somos, a que nos dedicamos

--- resources/views/store/partials/footer.blade.php
+++ resources/views/store/partials/footer.blade.php
@@ -2,13 +2,13 @@
 	<div class="row">
 		<div class="col-md-4">
 			<h3>Inteligent Container</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dolores ratione minus, voluptates dolore soluta harum. Nobis neque doloribus corrupti iusto rem beatae libero, incidunt animi nemo voluptatem dolores accusantium magni ad ipsum eum repellendus vero deleniti velit molestiae quaerat, veniam ducimus labore sapiente? Commodi ad, pariatur. Ab consectetur, quasi!</p>
+			<p class="tic">¡IntelligentContainer es una propuesta de nueva era para el uso de las tecnologias en el medio ambiente fomentando el reciclaje separando en cada uno de los contenedores!</p>
 		</div>
 		<div class="col-md-4">
 			<h3>Desarrollado por:</h3>
 			<div class="author-info">
-				<img src="https://pbs.twimg.com/profile_images/743519149897330692/eR4y5lk_.jpg" alt="" class="avatar">
-				<p><a href="#">@TICS</a> Ingenieros en tecnologias de la informacion y comunicacion, area sistemas informaticos
+				<img src="{{asset('image/Equipo.jpg')}}" alt="" class="avatar">
+				<p class="tic"><a href="#">@TICS</a> Ingenieros en tecnologias de la informacion y comunicacion, area sistemas informaticos
 				<li>Cristian Alberto Rosas Villalvazo</li>
 				<li>Jordan Axel Ramirez Ramirez</li>
 				<li>Jessica Rubi Rubio Alapizco</li>
